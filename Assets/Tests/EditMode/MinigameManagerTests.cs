ddac6ab469f97fe17bd4e6d8e7982b546de82a05
Destiny <dknapp91497@gmail.com>
2026-03-12 23:53:17 -0500

    Refresh docs for current life-loop and minigame systems

--- Assets/Tests/EditMode/MinigameManagerTests.cs
+++ Assets/Tests/EditMode/MinigameManagerTests.cs
@@ -15,6 +15,8 @@ namespace Survivebest.Tests.EditMode
 
             Assert.AreEqual(MinigameType.Surgery, manager.ResolveProfessionMinigame(ProfessionType.Doctor));
             Assert.AreEqual(MinigameType.RestaurantService, manager.ResolveProfessionMinigame(ProfessionType.Chef));
+            Assert.AreEqual(MinigameType.FirstAid, manager.ResolveProfessionMinigame(ProfessionType.Nurse));
+            Assert.AreEqual(MinigameType.EmergencyResponse, manager.ResolveProfessionMinigame(ProfessionType.Firefighter));
 
             Object.DestroyImmediate(go);
         }
@@ -33,5 +35,25 @@ namespace Survivebest.Tests.EditMode
 
             Object.DestroyImmediate(go);
         }
+        [Test]
+        public void GetAvailableMinigameTypes_IncludesKitchenFishingAndLeisureActivities()
+        {
+            GameObject go = new GameObject("MinigameManagerCatalog");
+            MinigameManager manager = go.AddComponent<MinigameManager>();
+
+            var types = manager.GetAvailableMinigameTypes();
+
+            Assert.IsTrue(types.Contains(MinigameType.Fishing));
+            Assert.IsTrue(types.Contains(MinigameType.Baking));
+            Assert.IsTrue(types.Contains(MinigameType.DrinkMixing));
+            Assert.IsTrue(types.Contains(MinigameType.MovieNight));
+            Assert.IsTrue(types.Contains(MinigameType.TVMarathon));
+            Assert.IsTrue(types.Contains(MinigameType.BookReading));
+            Assert.IsTrue(types.Contains(MinigameType.SingingSession));
+
+            Object.DestroyImmediate(go);
+        }
+
+
     }
 }
